49cd74a170bc2d86cca0c1f88b6a1ffac00feae6
Valentina E <tiyahelfrih@gmail.com>
2020-05-27 15:24:10 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,6 +56,16 @@ def over?(board)
   end
 end 
 
-def winner(board)
- 
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
 end
